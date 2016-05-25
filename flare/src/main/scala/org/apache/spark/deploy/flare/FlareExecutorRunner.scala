d80cf514513cd0f1dae97beb65176d5b2113ecb9
Amar Suhail <asuhail@nd.com>
2016-05-25 00:45:59 -0400

    flare node server implementation

---
+++ flare/src/main/scala/org/apache/spark/deploy/flare/FlareExecutorRunner.scala
@@ -0,0 +1,167 @@
+package org.apache.spark.deploy.flare
+
+import java.io.File
+
+import scala.collection.JavaConverters._
+import com.google.common.base.Charsets.UTF_8
+import com.google.common.io.Files
+import org.apache.spark.{Logging, SecurityManager, SparkConf}
+import org.apache.spark.deploy.{Command, ExecutorState}
+import org.apache.spark.deploy.worker.CommandUtils
+import org.apache.spark.flare.FlareClusterConfiguration
+import org.apache.spark.util.{ShutdownHookManager, Utils}
+import org.apache.spark.util.logging.FileAppender
+
+import scala.collection.mutable.HashMap
+
+class FlareExecutorRunner(
+    val clusterConf: FlareClusterConfiguration,
+    val executorId: String,
+    val cores: Int,
+    val memory: Long,
+    val sparkHome: File,
+    val executorDir: File,
+    conf: SparkConf,
+    securityManager: SecurityManager,
+    node: FlareNode)
+  extends Logging{
+  private var workerThread: Thread = _
+  private var process: Process = _
+  private var stdoutAppender: FileAppender = _
+  private var stderrAppender: FileAppender = _ 
+  
+  private val EXECUTOR_TERMINATE_TIMEOUT_MS = 10 * 1000
+
+  private var shutdownHook: AnyRef = _
+
+  @volatile var state: ExecutorState.Value = ExecutorState.LAUNCHING
+
+
+  private def getCommand(): Command = {
+    val args = Seq(
+      "--executor-id", executorId,
+      "--hostname", clusterConf.bindHostname,
+      "--port", clusterConf.bindPort.toString,
+      "--port-range", clusterConf.portRange.toString,
+      "--cores", cores.toString,
+      clusterConf.clusterUrl)
+    
+    val extraJavaOpts = conf.getOption("spark.executor.extraJavaOptions")
+      .map(Utils.splitCommandString).getOrElse(Seq.empty)
+    val classPathEntries = conf.getOption("spark.executor.extraClassPath")
+      .map(_.split(java.io.File.pathSeparator).toSeq).getOrElse(Nil)
+    val libraryPathEntries = conf.getOption("spark.executor.extraLibraryPath")
+      .map(_.split(java.io.File.pathSeparator).toSeq).getOrElse(Nil)
+    val sparkJavaOpts = Utils.sparkJavaOpts(conf, SparkConf.isExecutorStartupConf)
+    val javaOpts = sparkJavaOpts ++ extraJavaOpts
+    
+    val testingClassPath =
+      if (sys.props.contains("spark.testing")) {
+        sys.props("java.class.path").split(java.io.File.pathSeparator).toSeq
+      } else {
+        Nil
+      }
+    
+    val classPath = classPathEntries ++ testingClassPath
+    
+    val envVars = HashMap[String, String]()
+    
+    Command("org.apache.spark.executor.flare.FlareExecutorBackend", args, envVars, classPath, libraryPathEntries, javaOpts)
+  }
+  
+  def start() = {
+    workerThread = new Thread("flare-executor-runner-" + executorId) {
+      override def run() { fetchAndRunExecutor() }
+    }
+    workerThread.start()
+    
+    shutdownHook = ShutdownHookManager.addShutdownHook { () =>
+      if (state == ExecutorState.RUNNING) {
+        state = ExecutorState.FAILED
+      }
+      killProcess(Some("Flare executor shutting down"))
+    }
+  }
+
+  private def killProcess(message: Option[String]) = {
+    var exitCode: Option[Int] = None
+    if (process != null) {
+      logInfo("Killing process!")
+      if (stdoutAppender != null) {
+        stdoutAppender.stop()
+      }
+      if (stderrAppender != null) {
+        stderrAppender.stop()
+      }
+      exitCode = Utils.terminateProcess(process, EXECUTOR_TERMINATE_TIMEOUT_MS)
+      if (exitCode.isEmpty) {
+        logWarning("Failed to terminate process: " + process +
+          ". This process will likely be orphaned.")
+      }
+
+      node.onExecutorStateChanged(executorId, state, message, exitCode)
+    }
+    
+    //notify node
+  }
+  
+  def kill() = {
+    if (workerThread != null) {
+      workerThread.interrupt()
+      workerThread = null
+      state = ExecutorState.KILLED
+      
+      try {
+        ShutdownHookManager.removeShutdownHook(shutdownHook)
+      } catch {
+        case e: IllegalStateException => None
+      }
+    }
+  }
+  
+  def substituteVariables(argument: String): String = argument 
+  
+  private def fetchAndRunExecutor() = {
+    try {
+      val builder = CommandUtils.buildProcessBuilder(getCommand(), securityManager, memory.toInt, sparkHome.getAbsolutePath, substituteVariables)
+      val command = builder.command()
+      val formattedCommand = command.asScala.mkString("\"", "\" \"", "\"")
+      logInfo(s"Launch command: $formattedCommand")
+      
+      builder.directory(executorDir)
+      
+      builder.environment.put("SPARK_LAUNCH_WITH_SCALA", "0")
+
+      process = builder.start()
+      
+      val header = "Spark Executor Command: %s\n%s\n\n".format(
+        formattedCommand, "=" * 40)
+        
+      val stdout = new File(executorDir, "stdout")
+      stdoutAppender = FileAppender(process.getInputStream, stdout, conf)
+            
+      val stderr = new File(executorDir, "stderr")
+      Files.write(header, stderr, UTF_8)
+      stderrAppender = FileAppender(process.getErrorStream, stderr, conf)
+      
+      val exitCode = process.waitFor()
+      
+      state = ExecutorState.EXITED
+      val message = "Command exited with code " + exitCode
+
+      node.onExecutorStateChanged(executorId, state, Some(message), Some(exitCode))
+
+    } catch {
+      case interrupted: InterruptedException => {
+        logInfo("Runner thread for executor " + executorId + " interrupted")
+        state = ExecutorState.KILLED
+        killProcess(None)
+      }
+      case e: Exception => {
+        logError("Error running executor", e)
+        state = ExecutorState.FAILED
+        killProcess(Some(e.toString))
+      }
+    }
+  }
+}
---
+++ flare/src/main/scala/org/apache/spark/deploy/flare/FlareNode.scala
@@ -0,0 +1,151 @@
+package org.apache.spark.deploy.flare
+
+import java.io.{File, IOException}
+
+import org.apache.spark.deploy.ExecutorState
+
+import scala.collection.mutable.HashMap
+import org.apache.spark.{Logging, SecurityManager, SparkConf}
+import org.apache.spark.flare.{FlareCluster, _}
+import org.apache.spark.util.SignalLogger
+import org.jgroups.blocks.atomic.Counter
+
+class FlareNode(
+    clusterConf: FlareClusterConfiguration,
+    workDirPath: Option[String])
+  extends FlareClusterListener with Logging {
+  
+  private val testing: Boolean = sys.props.contains("spark.testing")
+  private val sparkHome =
+    if (testing) {
+      assert(sys.props.contains("spark.test.home"), "spark.test.home is not set!")
+      new File(sys.props("spark.test.home"))
+    } else {
+      new File(sys.env.get("SPARK_HOME").getOrElse(".."))
+    }
+  
+  val executors = new HashMap[String, FlareExecutorRunner]
+  
+  val cluster = FlareCluster(clusterConf)
+  var executorIdCounter: Counter = _
+
+  var workDir: File = null
+
+  val securityManager = new SecurityManager(new SparkConf)
+  
+  private def createWorkDir() {
+    workDir = workDirPath.map(new File(_)).getOrElse(new File(sparkHome, "work"))
+    try {
+      workDir.mkdirs()
+      if ( !workDir.exists() || !workDir.isDirectory) {
+        logError("Failed to create work directory " + workDir)
+        System.exit(1)
+      }
+      assert (workDir.isDirectory)
+    } catch {
+      case e: Exception =>
+        logError("Failed to create work directory " + workDir, e)
+        System.exit(1)
+    }
+  }
+  
+  private def joinCluster() = {
+    cluster.addListener(this)
+    cluster.connect()
+    executorIdCounter = cluster.counter("executorId")
+    cluster.send(NodeJoined(0))
+  }
+
+  override def onInitialize(initialize: Initialize) = {
+    logInfo("Driver initialized configuration, launching executors")
+    launchExecutors()
+  }
+
+  override def onDriverExited(driverExited: DriverExited): Unit = {
+    if (cluster.state.drivers.isEmpty) {
+      logInfo("All drivers have exited, terminating executors and uninitializing cluster")
+      cluster.state.uninitialize()
+      terminateExecutors()
+      executorIdCounter.set(0)
+      cluster.counter("driverId").set(0)
+    }
+  }
+
+  private def nextExecutorId(): Int = executorIdCounter.incrementAndGet.toInt
+  
+  private def launchExecutors() = {
+    if (cluster.state.isInitialized) {
+      logInfo(s"Configuration: ${cluster.state.properties}")
+      val conf = new SparkConf()
+      cluster.state.properties.foreach {
+        case (key, value) => conf.set(key, value)
+      }
+
+      val executorCount = conf.getInt("spark.flare.executorsPerNode", 1)
+
+      logInfo(s"Launching $executorCount executors")
+      for (i <- 0 until executorCount) {
+        val executorId = nextExecutorId.toString
+        launchExecutor(executorId, conf)
+      }
+    } else {
+      logInfo("Waiting for driver to initialize executors")
+    }
+  }
+  
+  private def launchExecutor(executorId: String, conf: SparkConf) = {
+    val appDir = new File(workDir, cluster.state.appId)
+    if (!appDir.exists() && !appDir.mkdir()) {
+      throw new IOException("Failed to create directory " + appDir)
+    }
+
+    val executorDir = new File(appDir, executorId.toString)
+    if (!executorDir.mkdirs()) {
+      throw new IOException("Failed to create directory " + executorDir)
+    }
+
+    val memory = conf.getSizeAsMb("spark.executor.memory", "1g")
+    val cores = conf.getInt("spark.executor.cores", 1)
+
+    val executor = new FlareExecutorRunner(clusterConf, executorId, cores, memory, sparkHome, executorDir, conf, securityManager, this)
+    executor.start()
+    executors(executorId) = executor
+  }
+
+  private def terminateExecutors() = {
+    for (executor <- executors.values) {
+      logInfo(s"Terminating executor ${executor.executorId}")
+      executor.kill()
+    }
+  }
+
+  def onExecutorStateChanged(executorId: String, state: ExecutorState.Value, message: Option[String], exitCode: Option[Int]): Unit = {
+    import ExecutorState._
+    state match {
+      case KILLED | FAILED | LOST | EXITED => {
+        executors.remove(executorId)
+        cluster.send(FlareExecutorLost(executorId, System.currentTimeMillis, s"Executor $state: $message"))
+      }
+      case _ =>
+    }
+  }
+
+  def start(): Unit = {
+    logInfo("Starting Flare Node")
+    logInfo(s"Running Spark version ${org.apache.spark.SPARK_VERSION}")
+    createWorkDir()
+    joinCluster()
+    launchExecutors() 
+  }
+}
+
+object FlareNode extends Logging {
+  def main(args: Array[String]) = {
+    SignalLogger.register(log)
+    
+    val nodeArgs = new FlareNodeArguments(args)
+    
+    new FlareNode(nodeArgs.clusterConf, Option(nodeArgs.workDir)).start()
+  }
+}
+
