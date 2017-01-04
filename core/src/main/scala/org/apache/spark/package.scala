e7fc6f978be1f03e4f0d11d0bcba0e377071b96d
jenkins-nd <jenkins@nd.com>
2017-01-04 02:09:20 +0000

    Setting version to 2.0.3+flare.13 for release

--- core/src/main/scala/org/apache/spark/package.scala
+++ core/src/main/scala/org/apache/spark/package.scala
@@ -45,7 +45,7 @@ import java.util.Properties
 
 package object spark {
   // For package docs only
-  // val SPARK_VERSION = "2.0.3+flare.13-SNAPSHOT"
+  // val SPARK_VERSION = "2.0.3+flare.13"
 
   private object SparkBuildInfo {
 
