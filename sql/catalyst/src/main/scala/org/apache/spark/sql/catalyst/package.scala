5d611905e983bef4959b6dc6b954ec99ad8e3608
Rakesh Chalasani <rchalasani@nd.com>
2018-04-25 15:26:03 -0400

    Avoid style changes

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/package.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/package.scala
@@ -23,7 +23,9 @@ package org.apache.spark.sql
  */
 package object catalyst {
   /**
-    * A fix for Spark Cassandra connector as it is not compatible with Sprk v2.3
+    * A JVM-global lock that should be used to prevent thread safety issues when using things in
+    * scala.reflect.*.  Note that Scala Reflection API is made thread-safe in 2.11, but not yet for
+    * 2.10.* builds.  See SI-6240 for more details.
     */
   protected[sql] object ScalaReflectionLock
 }
